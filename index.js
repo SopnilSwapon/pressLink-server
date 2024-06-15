89f66dd1fa646c29a28475ed17d813f478e7b84e
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-06-15 13:25:41 +0600

    added update payment api

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const  cors = require('cors');
 const port = process.env.PORT || 4000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 app.use(cors());
 app.use(express.json());
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nshaxle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -37,7 +37,22 @@ app.post('/users', async(req, res) =>{
   }
   const result = await usersCollection.insertOne(user);
   res.send(result);
+});
+
+app.put('/user/:email', async(req, res) =>{
+  const email = req.params.email;
+  const userInfo = req.body;
+  const options = {upsert: true}
+  const query = {email};
+  const updateUserInfo = {
+    $set:{
+        ...userInfo
+    }
+    } 
+  const result = await usersCollection.updateOne(query, updateUserInfo, options);
+  res.send(result)
 })
+
 app.get('/users', async (req, res) =>{
   const result = await usersCollection.find().toArray();
   res.send(result)
@@ -200,7 +215,37 @@ app.put('/user/role/:email', async (req, res) =>{
 
       const result = await paymentCollection.insertOne(paymentInfo);
       res.send(result)
-    })
+    });
+    // payment intent 
+    app.post('/create-payment-intent', async (req, res) =>{
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+    app.get('/payment/:email', async (req, res) =>{
+      const email = req.params.email;
+      const query = {user: email, status: 'pending'};
+      const result = await paymentCollection.findOne(query)
+      res.send(result)
+    });
+
+    app.patch('/payment/:email', async (req, res) => {
+      const paymentInfo = req.body;
+      const filter = { user: req.params.email, status: 'pending' };
+      const options = { upsert: true };
+      const updatedInfo = { $set: paymentInfo };
+  
+      const result = await paymentCollection.updateOne(filter, updatedInfo, options);
+  
+      res.send(result);
+  });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
